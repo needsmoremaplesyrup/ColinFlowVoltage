6c2ea523350d2c73ea44bc769e4917819961b1df
ColinParkyn <59575068+ColinParkyn@users.noreply.github.com>
2022-05-27 15:20:45 -0700

    live done

--- main.py
+++ main.py
@@ -1,44 +1,28 @@
 import pyvisa as visa
 import time
-from matplotlib.pyplot import ylabel, plot, show, xlabel
+import numpy as np
+import matplotlib.pyplot as plt
+
+import random
 
 def voltagetracker(interval,duration):#min,min
-    print("hi")
-    x = []
-    data = []
-    print(interval,duration)
-    rm = visa.ResourceManager()
-    print(rm.list_resources())
+    duration=duration*60
+    interval=interval*60
+    mins=[]
+    data=[]
+    total={}
+    #rm = visa.ResourceManager()
+    #print(rm.list_resources())
     #Keithley = rm.open_resource('USB0::0x05E6::0x2280::4386872::INSTR')
-    for i in range(int(duration/interval)+1):
-        x.append(interval*i)
-        data.append(i)#Keithley.query('V?'))
-        time.sleep(interval)
-    plot(x, data)
-    xlabel('time (min)')
-    ylabel('voltage (V)')
-    show()
-'''
-class voltagetracker:#min,min
-    def __init__(self,interval,duration,x,data):
-        print(interval,duration)
-        interval=interval
-        duration=duration
-        x = []
-        data = []
-    print(self.interval,self.duration)
-    rm = visa.ResourceManager()
-    rm.list_resources()
-    #Keithley = rm.open_resource('GPIB0::xx::INSTR')
-
-    for i in (self.duration/interval):
-        time.sleep(interval*60)
-        self.x.append(interval*i)
-        self.data.append(Keithley.query('V?'))
-    plt.plot(self.time,self.data)
-    plt.xlabel('time (min)')
-    plt.ylabel('voltage (V)')
+    for i in range(int(duration/interval+1)):
+        plt.clf()
+        mins.append(interval*i)
+        data.append(random.randint(1,100))
+        #data[i]=random.randint(1,100)#Keithley.query('V?'))
+        plt.plot(mins,data)
+        plt.pause(interval)
+    plt.title('Voltage Tracker')
+    plt.xlabel('Time (min)')
+    plt.ylabel('Voltage (V)')
     plt.show()
-'''
-print("run")
-voltagetracker(1,6)
+voltagetracker(1/60,1)
