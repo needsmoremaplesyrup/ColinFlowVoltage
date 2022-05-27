b9136110f3c752dc78e229601aea3d190528947e
ColinBerlincomp <cparkyn@ymail.com>
2022-05-27 13:51:32 -0700

    got address

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
 import pyvisa as visa
 import time
-import numpy
-import matplotlib.pyplot as plt
-'''
-x=10
-y=66
+from matplotlib.pyplot import ylabel, plot, show, xlabel
 
-for i in range(int(y/x)):
-    print(i)
-
-'''
 def voltagetracker(interval,duration):#min,min
+    print("hi")
     x = []
     data = []
     print(interval,duration)
     rm = visa.ResourceManager()
-    rm.list_resources()
-    #Keithley = rm.open_resource('GPIB0::xx::INSTR')
-
+    print(rm.list_resources())
+    #Keithley = rm.open_resource('USB0::0x05E6::0x2280::4386872::INSTR')
     for i in range(int(duration/interval)+1):
         x.append(interval*i)
         data.append(i)#Keithley.query('V?'))
         time.sleep(interval)
-    plt.plot(x,data)
-    plt.xlabel('time (min)')
-    plt.ylabel('voltage (V)')
-    plt.show()
+    plot(x, data)
+    xlabel('time (min)')
+    ylabel('voltage (V)')
+    show()
 '''
 class voltagetracker:#min,min
     def __init__(self,interval,duration,x,data):
@@ -48,5 +40,5 @@ class voltagetracker:#min,min
     plt.ylabel('voltage (V)')
     plt.show()
 '''
-
+print("run")
 voltagetracker(1,6)
