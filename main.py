600bea30b72e1b2d305a46a9fce2b6714babca83
ColinParkyn <59575068+ColinParkyn@users.noreply.github.com>
2022-05-30 16:08:13 -0700

    query

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import pyvisa as visa
-import time
 from datetime import datetime
 import os
-import io
-import numpy as np
 import matplotlib.pyplot as plt
-import random
 
 def voltagetracker(voltage,current,interval,duration):#min,min
-    duration=duration
-    interval=interval
+    duration=duration*60
+    interval=interval*60
     mins=[]
     data=[]
 
@@ -25,13 +21,15 @@ def voltagetracker(voltage,current,interval,duration):#min,min
     datafile.write("Time(X), Voltage(V) \n")
 
     Keithley = rm.open_resource('USB0::0x05E6::0x2280::4386872::INSTR')
+    print(Keithley.query("*IDN?"))
+    Keithley.write(":SENSe:CURRent:RANGe:AUTO ON")
     #Keithley.write(":FORMat:ELEMents \"MODE, CC\"")
     #Keithley.write(f":VOLT {voltage}")
     #Keithley.write(f":CURR {current}")
     for i in range(int(duration/interval+1)):
         plt.clf()
         mins.append(interval*i)
-        data.append(Keithley.read("SENS:FUNC \"VOLT\""))
+        data.append(Keithley.query("SENS:FUNC \"VOLT\"?"))
         datafile.write(f"{mins[-1]},{data[-1]}\n")
         plt.plot(mins,data)
         plt.pause(interval)
