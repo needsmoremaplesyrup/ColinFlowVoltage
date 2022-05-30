eb334ec916a5f9416d58c913f7e2435fc1ef8e90
ColinParkyn <59575068+ColinParkyn@users.noreply.github.com>
2022-05-30 12:48:53 -0700

    Data storage, live version for keithley

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pyvisa as visa
 import time
+from datetime import datetime
+import os
+import io
 import numpy as np
 import matplotlib.pyplot as plt
-
 import random
 
 def voltagetracker(interval,duration):#min,min
@@ -10,17 +12,25 @@ def voltagetracker(interval,duration):#min,min
     interval=interval*60
     mins=[]
     data=[]
-    total={}
-    #rm = visa.ResourceManager()
-    #print(rm.list_resources())
-    #Keithley = rm.open_resource('USB0::0x05E6::0x2280::4386872::INSTR')
+
+    try:
+        os.chdir(f"C:\\Users\\{os.getlogin()}\\Documents\\VoltageDatafiles")
+    except:
+        os.mkdir(f"C:\\Users\\{os.getlogin()}\\Documents\\VoltageDatafiles")
+    timestamp=datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+    rm = visa.ResourceManager()
+    print(rm.list_resources())
+    datafile=open(f"{timestamp}.csv","w")
+    datafile.write("Time(X), Voltage(V) \n")
+    Keithley = rm.open_resource('USB0::0x05E6::0x2280::4386872::INSTR')
     for i in range(int(duration/interval+1)):
         plt.clf()
         mins.append(interval*i)
-        data.append(random.randint(1,100))
-        #data[i]=random.randint(1,100)#Keithley.query('V?'))
+        data.append(Keithley.query("V?"))
+        datafile.write(f"{mins[-1]},{data[-1]}\n")
         plt.plot(mins,data)
         plt.pause(interval)
+    datafile.close()
     plt.title('Voltage Tracker')
     plt.xlabel('Time (min)')
     plt.ylabel('Voltage (V)')
