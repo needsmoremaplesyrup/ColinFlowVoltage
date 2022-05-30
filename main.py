95bada7a237ed17545b8f565aa46d34a0f960d64
ColinBerlincomp <cparkyn@ymail.com>
2022-05-30 15:45:40 -0700

    comment

--- main.py
+++ main.py
@@ -20,18 +20,18 @@ def voltagetracker(voltage,current,interval,duration):#min,min
 
     timestamp=datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
     rm = visa.ResourceManager()
-    #print(rm.list_resources())
+    print(rm.list_resources())
     datafile=open(f"{timestamp}.csv","w")
     datafile.write("Time(X), Voltage(V) \n")
 
     Keithley = rm.open_resource('USB0::0x05E6::0x2280::4386872::INSTR')
-    Keithley.write(":FORMat:ELEMents \"MODE, CC\"")
-    Keithley.write(f":VOLT {voltage}")
-    Keithley.write(f":CURR {current}")
+    #Keithley.write(":FORMat:ELEMents \"MODE, CC\"")
+    #Keithley.write(f":VOLT {voltage}")
+    #Keithley.write(f":CURR {current}")
     for i in range(int(duration/interval+1)):
         plt.clf()
         mins.append(interval*i)
-        data.append(Keithley.query("SENS:FUNC \"VOLT\""))
+        data.append(Keithley.read("SENS:FUNC \"VOLT\""))
         datafile.write(f"{mins[-1]},{data[-1]}\n")
         plt.plot(mins,data)
         plt.pause(interval)
