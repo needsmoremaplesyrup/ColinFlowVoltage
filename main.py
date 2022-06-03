6e3077950b9c5b5f7af79d8c1c26d69f3810c33c
ColinParkyn <59575068+ColinParkyn@users.noreply.github.com>
2022-06-03 12:28:31 -0700

    comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os
 import matplotlib.pyplot as plt
 
-def voltagetracker(voltage,current,interval,duration):#V,A,min,min
+def tracker(voltage,current,interval,duration):#V,A,min,min
     voltdata = []
     currdata = []
     timedata = []
@@ -33,7 +33,7 @@ def voltagetracker(voltage,current,interval,duration):#V,A,min,min
 
     for i in range(int(duration/interval+1)):
         plt.clf()
-        x,y,z=Keithley.query(":MEAS:VOLT?").split(",")
+        x,y,z=Keithley.query(":MEAS:VOLT?").split(",")#idk why i asks for VOLT and get TIME,VOLT,CURR
         x,y,z=(float(x[1:-1]),float(y[1:-1]),float(z[1:-1]))
         if i==0:
             start=z
@@ -44,13 +44,15 @@ def voltagetracker(voltage,current,interval,duration):#V,A,min,min
         datafile.write(f"{timedata[-1]},{currdata[-1]},{voltdata[-1]}\n")
         if duration>=3*60:
             timedata[-1]=round(timedata[-1]/60)
-        plt.plot(timedata,voltdata)
+        plt.plot(timedata,voltdata,'o')
         plt.pause(interval)
 
     datafile.close()
+    #select V or A, or have both as subplots side by side
     plt.title('Voltage Tracker')
     plt.xlabel('Time (sec)')
     plt.ylabel('Voltage (V)')
+    #save graph as image
     plt.show()
 
-voltagetracker(30,.26,1/60,1/6)#Voltage(V),Current(A),Interval length(X), Duration(X)
+tracker(30,.26,1/60,1/6)#Voltage(V),Current(A),Interval length(X), Duration(X)
