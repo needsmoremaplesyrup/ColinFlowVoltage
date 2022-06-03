c425b596cc363ffbd43b92396048fb05f708593a
ColinParkyn <59575068+ColinParkyn@users.noreply.github.com>
2022-06-03 11:58:05 -0700

    adjusts for time using data's values

--- main.py
+++ main.py
@@ -1,54 +1,56 @@
 import pyvisa as visa
-import time
 from datetime import datetime
 import os
-import io
-import numpy as np
 import matplotlib.pyplot as plt
-import random
 
-def voltagetracker(voltage,current,interval,duration):#min,min
+def voltagetracker(voltage,current,interval,duration):#V,A,min,min
+    voltdata = []
+    currdata = []
+    timedata = []
+    x, y, z, start = (0, 0, 0, 0)
+
+    #Convert minutes into seconds
     duration=duration*60
     interval=interval*60
-    mins=[]
-    voltdata=[]
-    currdata=[]
-    timedata=[]
 
+    #check for file folder, if it doesn't exist make it
     try:
         os.chdir(f"C:\\Users\\{os.getlogin()}\\Documents\\VoltageDatafiles")
     except:
         os.mkdir(f"C:\\Users\\{os.getlogin()}\\Documents\\VoltageDatafiles")
 
+    #find date & time then create file and write header in file
     timestamp=datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    rm = visa.ResourceManager()
     datafile=open(f"{timestamp}.csv","w")
-    datafile.write("Time(X), Current(A), Voltage(V), Time(s)\n")
+    datafile.write("Time(s), Current(A), Voltage(V)\n")
+
+    #Start communicating with device
+    rm = visa.ResourceManager()
     Keithley = rm.open_resource('USB0::0x05E6::0x2280::4386872::INSTR')
     Keithley.read_termination = '\n'
     Keithley.write_termination = '\n'
+    Keithley.query("*IDN?")
+
     for i in range(int(duration/interval+1)):
-        x=0
-        y=0
-        z=0
         plt.clf()
-        mins.append(interval*i)
-        #hold=Keithley.query(":MEAS:VOLT?")
         x,y,z=Keithley.query(":MEAS:VOLT?").split(",")
-        x=float(x[1:-1])
-        y=float(y[1:-1])
-        z=float(z[1:-1])
+        x,y,z=(float(x[1:-1]),float(y[1:-1]),float(z[1:-1]))
+        if i==0:
+            start=z
+        z=z-start
         currdata.append(x)
         voltdata.append(y)
         timedata.append(z)
-        #stagger time based on initial timepoint
-        datafile.write(f"{mins[-1]},{currdata[-1]},{voltdata[-1]},{timedata[-1]}\n")
-        plt.plot(mins,voltdata)
+        datafile.write(f"{timedata[-1]},{currdata[-1]},{voltdata[-1]}\n")
+        if interval>=60:
+            timedata[-1]=round(timedata[-1]/60)
+        plt.plot(timedata,voltdata)
         plt.pause(interval)
+
     datafile.close()
     plt.title('Voltage Tracker')
     plt.xlabel('Time (sec)')
     plt.ylabel('Voltage (V)')
     plt.show()
 
-voltagetracker(0,.26,1/60,1/6)#Voltage(V),Current(A),Interval length(X), Duration(X)
+voltagetracker(30,.26,1/60,1/6)#Voltage(V),Current(A),Interval length(X), Duration(X)
