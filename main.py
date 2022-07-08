c8b1a50caabd7229ac441cf1bac87b3859b7f11c
ColinParkyn <59575068+ColinParkyn@users.noreply.github.com>
2022-07-08 14:49:36 -0700

    comment changes, replaced except with finally

--- main.py
+++ main.py
@@ -4,94 +4,92 @@ import os
 import matplotlib.pyplot as plt
 
 
-
 def tracker():
-    #data variables
+    # data variables
     voltdata = []
     currdata = []
     timedata = []
     x, y, z, start = (0, 0, 0, 0)
     minsec = "sec"
 
-    #check for datafile folder, if it doesn't exist make it
+# check for datafile folder, if it doesn't exist make it
     try:
         os.chdir(f"C:\\Users\\{os.getlogin()}\\Documents\\VoltageDatafiles")
-    except:
+    finally:
         os.mkdir(f"C:\\Users\\{os.getlogin()}\\Documents\\VoltageDatafiles")
 
-    #name data file, if no name entered use date and time
-    name=input("Type name of file: ")
-    if(name==""):
-        name=datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+# name data file, if no name entered use date and time
+    name = input("Type name of file: ")
+    if name == "":
+        name = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
     else:
         pass
 
-    #open data file and write headings
+# open data file and write headings
     datafile = open(f"{name}.csv", "w")
     datafile.write("Time(s), Voltage(V), Current(A)\n")
 
+# Get time values for duration of run and interval between measurements
+    duration = input("Duration (min): ")
+    interval = input("Intervals (min): ")
+    if duration == "":
+        duration = 120
+    if interval == "":
+        interval = .25
 
-    #Get time values for duration of run and interval between measurements
-    duration=input("Duration (min): ")
-    interval=input("Intervals (min): ")
-    if duration=="":
-        duration=120
-    if interval=="":
-        interval=.25
-
-    #adjust values into seconds
+# adjust values into seconds
     duration = duration*60
     interval = interval*60
 
-    #if duration is longer than 3 min use minutes rather than seconds in the graphing
+# if duration is longer than 3 min use minutes rather than seconds in the graphing
     if duration > 180:
         minsec = "min"
 
-    #input voltage and currents
-    voltage=input("Input voltage(V): ")
-    current=input("Input current(A): ")
-    if voltage=="":
-        voltage=30
-    if current=="":
-        current=.26
+# input voltage and currents
+    voltage = input("Input voltage(V): ")
+    current = input("Input current(A): ")
+    if voltage == "":
+        voltage = 30
+    if current == "":
+        current = .26
 
-    #Start communicating with device
+# Start communicating with device
     rm = visa.ResourceManager()
     Keithley = rm.open_resource('USB0::0x05E6::0x2280::4386872::INSTR')
     Keithley.read_termination = '\n'
     Keithley.write_termination = '\n'
 
-    #check connections
+# check connections
     Keithley.query("*IDN?")
 
     try:
-        #set parameters on device and turn device on
+        # set parameters on device and turn device on
         Keithley.write(f":VOLT {str(voltage)}")
         Keithley.write(f":CURR {str(current)}")
         Keithley.write(":INIT:CONT ON")
         for i in range(int(duration/interval+1)):
             plt.clf()
-            x, y, z = Keithley.query(":MEAS:VOLT?").split(",")  #idk why i asks for VOLT and get TIME,VOLT,CURR
-            #Remove + and unit from data
+            x, y, z = Keithley.query(":MEAS:VOLT?").split(",")  # idk why i asks for VOLT and get TIME,VOLT,CURR
+            # Remove + and unit from data
             x, y, z = (float(x[1:-1]), float(y[1:-1]), float(z[1:-1]))
-            #adjusting time data to use it for timepoint from initial start
+            # adjusting time data to use it for timepoint from initial start
             if i == 0:
                 start = z
             z = round(z-start, 5)
-            #add to lists
+            # add to lists
             currdata.append(x)
             voltdata.append(y)
             timedata.append(z)
-            #write data to file
+            # write data to file
             datafile.write(f"{timedata[-1]},{voltdata[-1]},{currdata[-1]}\n")
-            #adjusts time units if duration is too long for seconds to make sense
+            # adjusts time units if duration is too long for seconds to make sense
             if duration >= 180:
                 timedata[-1] = round(timedata[-1]/60, 1)
             plt.plot(timedata, voltdata)
             plt.pause(interval)
 
-    except:
-        #if error occurs, turn power supply off, save the graph
+    finally:
+        # if error occurs, turn power supply off, save the graph
         Keithley.write(":OUTP OFF")
         Keithley.write(":VOLT 0")
         Keithley.write(":CURR 0")
@@ -99,15 +97,15 @@ def tracker():
         plt.savefig(f"{name}.png")
 
     datafile.close()
-    ##turn power supply off
+    # turn power supply off
     Keithley.write(":OUTP OFF")
     Keithley.write(":VOLT 0")
     Keithley.write(":CURR 0")
-    ##select V or A, or have both as subplots side by side
+    # select V or A, or have both as subplots side by side
     plt.title('Voltage')
     plt.xlabel(f'Time ({minsec})')
     plt.ylabel('Voltage (V)')
-    #save graph as image
+    # save graph as image
     plt.show()
     plt.savefig(f"{name}.png")
 
